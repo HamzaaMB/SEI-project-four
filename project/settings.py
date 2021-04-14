8bb45bb3ca33c75e42c8b4756cbe052aaab108bf
Hamza Butt <hamzambutt1@gmail.com>
2021-04-14 21:10:45 +0100

    PosgreSQL database connected

--- project/settings.py
+++ project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'food-nutrition',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
