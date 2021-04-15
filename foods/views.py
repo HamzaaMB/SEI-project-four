a794f7b056781d6b41225c87502477f5a3226dfe
Hamza Butt <hamzambutt1@gmail.com>
2021-04-15 15:19:43 +0100

    food detail added in views, which shows individual food. url added and serialized

--- foods/views.py
+++ foods/views.py
@@ -9,5 +9,15 @@ class FoodListView(APIView):
 
     def get(self, _request):
         foods = Food.objects.all() #This returns everything from the database.
-        serialised_foods = FoodSerializer(foods, many=True) # This converts the data.
-        return Response(serialised_foods.data, status = status.HTTP_200_OK)
+        serialized_foods = FoodSerializer(foods, many=True) # This converts the data.
+        return Response(serialized_foods.data, status = status.HTTP_200_OK)
+
+
+class FoodDetailView(APIView):
+
+    def get(self, _request, pk):
+        foods = Food.objects.get(pk=pk)
+        serialized_foods = FoodSerializer(foods)
+        return Response(serialized_foods.data, status=status.HTTP_200_OK)
+
+
