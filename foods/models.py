023d1aafb3ae74ef226e063d23e4e298a8a1b58c
Hamza Butt <hamzambutt1@gmail.com>
2021-04-15 13:56:02 +0100

    model complete and tested in django aministrator, entries in postgresql showing

--- foods/models.py
+++ foods/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 class Food(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     calories = models.CharField(max_length=50)
+    protein = models.CharField(max_length=50)
+    carbs = models.CharField(max_length=50)
+    fat = models.CharField(max_length=50)
+    weight = models.CharField(max_length=50)
+    serving = models.CharField(max_length=50)
+    servingAs = models.CharField(max_length=50)
 
-
+    def __str__(self):
+        return f"{self.title} - {self.calories}"
