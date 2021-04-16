ebd0d4e2ceb7456c7ff5146ccf83830a3a77308c
Hamza Butt <hamzambutt1@gmail.com>
2021-04-16 16:56:59 +0100

    all corresponding model relationships are complete

--- jwt_auth/models.py
+++ jwt_auth/models.py
@@ -7,3 +7,7 @@ class user(AbstractUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     profile_image= models.CharField(max_length=300)
+
+
+    def __str__(self):
+        return f"{self.first_name}"
--- logbook/models.py
+++ logbook/models.py
@@ -1,13 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class Logbook(models.Model):
-    food = models.ForeignKey(
-        "foods.Food",
-        related_name="foods",
-        on_delete= models.CASCADE
+    name = models.CharField(max_length=50)
+    food = models.ManyToManyField('foods.Food', related_name='logbooks')
+    owner = models.ForeignKey(
+        User,
+        related_name='logbooks',
+        on_delete = models.CASCADE
     )
 
     def __str__(self):
-        return f"{self.food}"
-
+        return f"{self.name}"
