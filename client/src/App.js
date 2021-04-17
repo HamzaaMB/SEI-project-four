46c2a1fbc9c1ad84057aa5972547f2751aa25c67
Hamza Butt <hamzambutt1@gmail.com>
2021-04-18 00:17:51 +0100

    api tested and renders on page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <h1>Hello World</h1>
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [foods, setFoods] = useState(null)
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios.get('api/foods/')
+      setFoods(data)
+    }
+    getData()
+  }, [])
+  console.log(foods)
+
+  if (!foods) return null
+  return (
+  
+    <h1>{foods[0].title}</h1>
+  )
 }
 
 export default App
+
