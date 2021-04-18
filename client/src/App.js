470a696bd471b3603125ed8941cf9fb133763d5c
Hamza Butt <hamzambutt1@gmail.com>
2021-04-18 19:34:38 +0100

    created login page with handle submit and change functions, post request for login information also added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
 import Homepage from './components/Homepage'
 import Foodpage from './components/Foodpage'
+import Login from './components/auth/Login'
+
 
 const App = () => {
 
 
   return (
     <>
-    
-      <Homepage />
-      <Foodpage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path ="/">
+            <Homepage />
+          </Route>
+          <Route path ="/foods">
+            <Foodpage />
+          </Route>
+          <Route path = "/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      
+
 
     </>
   )
