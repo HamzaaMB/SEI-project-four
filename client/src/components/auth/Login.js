84b722eaa539499ad6cc8f6806967d61c2692aab
Hamza Butt <hamzambutt1@gmail.com>
2021-04-18 20:01:29 +0100

    login page lets the user login and redirects to home page - working

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-// import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 
@@ -9,7 +9,7 @@ const Login = () => {
     email: '',
     password: '',
   })
-  // const history = useHistory()
+  const history = useHistory()
 
   const handleChange = event => {
     const newFormData = { ...formData, [event.target.name]: event.target.value }
@@ -19,56 +19,37 @@ const Login = () => {
 
   const handleSubmit = async event => {
     event.preventDefault()
-    console.log('Token ->')
-    const response = await axios.post('api/login,', formData)
+    const response = await axios.post('api/auth/login/', formData)
     window.localStorage.setItem('token', response.data.token)
+    history.push('/')
 
     console.log(response)
   }
 
 
   return (
-    <div className="login">
-      <section className="section" id="login-section">
-        <div className="container">
-          <div className="columns">
-            <form onSubmit={handleSubmit}
-            >
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    placeholder="Email"
-                    name="email"
-                    value={formData.email}
-                    onChange={(event) => handleChange(event)}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label">Password</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={formData.password}
-                    onChange={(event) => handleChange(event)}
-                  />
-                </div>
-              </div>
-              <div className="field">
-                {/* <Link to={`/${formData.username}`}> */}
-                <button type="submit" className="button register-button">Log Me In!</button>
-                {/* </Link> */}
-              </div>
-            </form>
-          </div>
-        </div >
-      </section >
-    </div>
+
+    <form onSubmit={handleSubmit}>
+      <label className="label">Email</label>
+      <input
+        className="input"
+        placeholder="Email"
+        name="email"
+        value={formData.email}
+        onChange={(event) => handleChange(event)}
+      />
+              
+      <label className="label">Password</label>
+      <input
+        className="input"
+        type="password"
+        placeholder="Password"
+        name="password"
+        value={formData.password}
+        onChange={(event) => handleChange(event)}
+      />
+      <button type="submit" className="button register-button">Log Me In!</button>
+    </form>
   )
 
 
