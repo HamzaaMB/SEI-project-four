74bb1dbf26fcff3286524eec3ed1437d62e92728
Hamza Butt <hamzambutt1@gmail.com>
2021-04-20 00:10:39 +0100

    changes to navbar require overriding bootstrap css, commit without those changes

--- client/src/components/MyNavbar.js
+++ client/src/components/MyNavbar.js
@@ -30,23 +30,25 @@ const MyNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <div className="nav-item-left">
-              <Nav.Link href="/foods">Food</Nav.Link>
-              <Nav.Link href="/about">About</Nav.Link>
-            </div>
-            <div className="nav-item-right">
-              {userIsAuthenticated() &&
+            <div className="main-navbar">
+              <div className="nav-item-left">
+                <Nav.Link href="/foods">Food</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+              </div>
+              <div className="nav-item-right">
+                {userIsAuthenticated() &&
           <>
             <Nav.Link href="/logbook">Logbook</Nav.Link>
             <Nav.Link onClick = {handleLogout}>Logout</Nav.Link>
           </>  
-              }
-              {!userIsAuthenticated() &&
+                }
+                {!userIsAuthenticated() &&
           <> 
             <Nav.Link className="border-left pl-2 ml-auto" href="/login">Login</Nav.Link>
             <Nav.Link className="border-left pl-2 ml-auto" href="/register">Register</Nav.Link>
           </>
-              }
+                }
+              </div>
             </div>
           </Nav>  
         </Navbar.Collapse>
