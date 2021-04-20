e19a478fae56ee005077adffddf3f31c9c78b0f9
Hamza Butt <hamzambutt1@gmail.com>
2021-04-20 22:12:49 +0100

    food index page css first draft complete

--- client/src/components/FoodIndex.js
+++ client/src/components/FoodIndex.js
@@ -25,9 +25,11 @@ const FoodIndex = () => {
           return <Link to={`/foods/${food.id}`} key={food.id}>
             <div className="results-container">
               <ul className="food-list">
-                <li><h4 className="title-search">{food.title}</h4></li>
-                <li><h6 className="serving-search">{food.serving} {food.servingAs}, {food.weight}g</h6></li>
-                <li><h6 className="macros-search">Carbs: {food.carbs} | Protein: {food.protein} | Fat: {food.fat}</h6></li>
+                <div className="food-results">
+                  <li><h4 className="title-search">{food.title}</h4></li>
+                  <li><h6 className="serving-search">{food.serving} {food.servingAs}, {food.weight}g</h6></li>
+                  <li><h6 className="macros-search">Carbs: {food.carbs} | Protein: {food.protein} | Fat: {food.fat}</h6></li>
+                </div>
               </ul>
             </div>
           </Link>
