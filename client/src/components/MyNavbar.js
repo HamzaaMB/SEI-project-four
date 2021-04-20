be75d27c4205475648f7879a7c712cc42c21721e
Hamza Butt <hamzambutt1@gmail.com>
2021-04-20 22:37:13 +0100

    changes made to navbar

--- client/src/components/MyNavbar.js
+++ client/src/components/MyNavbar.js
@@ -26,30 +26,25 @@ const MyNavbar = () => {
       <div className="logo">
         <p>My Foodie Pal?</p>
       </div>
-      <Navbar style={{ width: '100%', margin: 'auto' }} bg="primary" expand="lg" variamt="dark"> 
+      <Navbar style={{ paddingLeft: '185px' }} bg="primary" variamt="dark"> 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
-            <div className="main-navbar">
-              <div className="nav-item-left">
-                <Nav.Link href="/foods">Food</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-              </div>
-              <div className="nav-item-right">
-                {userIsAuthenticated() &&
+            <Nav.Link href="/foods">Food</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            {userIsAuthenticated() &&
           <>
             <Nav.Link href="/logbook">Logbook</Nav.Link>
             <Nav.Link onClick = {handleLogout}>Logout</Nav.Link>
           </>  
-                }
-                {!userIsAuthenticated() &&
+            }
+            {!userIsAuthenticated() &&
           <> 
-            <Nav.Link className="border-left pl-2 ml-auto" href="/login">Login</Nav.Link>
-            <Nav.Link className="border-left pl-2 ml-auto" href="/register">Register</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
           </>
-                }
-              </div>
-            </div>
+            }
+
           </Nav>  
         </Navbar.Collapse>
       </Navbar>
