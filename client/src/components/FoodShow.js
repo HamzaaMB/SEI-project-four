43102252064e1bb6b0fc27f3119146d081014907
Hamza Butt <hamzambutt1@gmail.com>
2021-04-20 15:41:08 +0100

    post and delete added per user request - only users the logbooks belong to can carry out requests

--- client/src/components/FoodShow.js
+++ client/src/components/FoodShow.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-
+import { userIsOwner } from './auth/helpers/auth'
 // import FoodIndex from './FoodIndex'
 
 
 const FoodShow = () => {  
 
 
+  const owner = userIsOwner()
+  console.log('owner', owner)
   
   const params = useParams()
   console.log(params.id)
@@ -20,13 +22,20 @@ const FoodShow = () => {
     }
     getData()
   }, [])
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const response = await axios.get('api/logbooks')
+  //   }
+  // })
   
 
   if (!food) return null
   console.log(food)
+
   
   
-  return <p>Working new page</p>
+  return <p></p>
   
 }
 
--- logbook/views.py
+++ logbook/views.py
@@ -2,40 +2,34 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.exceptions import NotFound, PermissionDenied
-# from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Logbook
+from .serializers.common import LogbookSerializer
 from .serializers.populated import PopulatedLogbookSerializer
 
 
-class LogbookListView(APIView):
-    # permission_classes = (IsAuthenticated,)
 
+class LogbookListView(APIView): 
+    # permission_classes = (IsAuthenticated,)
     def get(self, _request):
         logbooks = Logbook.objects.all()
         serialized_logbooks = PopulatedLogbookSerializer(logbooks, many=True)
         return Response(serialized_logbooks.data, status=status.HTTP_200_OK)
 
 
-    def post(self, request):
-        logbook_to_add = PopulatedLogbookSerializer(data=request.data)
+
+class LogbookDetailView(APIView): #individual logbook to GET on front end and POST and DELETE.
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk):
+        request.data["owner"] = request.user.id
+        logbook_to_add = LogbookSerializer(data=request.data)
         if logbook_to_add.is_valid():
             logbook_to_add.save()
             return Response(logbook_to_add.data, status=status.HTTP_201_CREATED)
         return Response(logbook_to_add.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-
-class LogbookDetailView(APIView):
-
-    # permission_classes = (IsAuthenticated,)
-
-    def get(self, request, pk):
-        
-        user = request.data.get('id')
-        logbook = Logbook.objects.get(pk=pk)
-        
-        serialized_logbook = PopulatedLogbookSerializer(logbook)
-        return Response(serialized_logbook.data, status=status.HTTP_200_OK)
 
     def delete(self, request, pk):
         try:
@@ -48,6 +42,15 @@ class LogbookDetailView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-    # def post(self, request, pk):
-    #Might need to revisit this after testing in the front-end. Requirement for user posting per 'logbook id' -> so the user can add food to a specific logbook.
 
+    # def post(self, request):
+    #     request.data["owner"] = request.user.id
+
+
+    # def get(self, request, pk):
+        
+        # user = request.data.get('id')
+        # logbook = Logbook.objects.get(pk=pk)
+        
+        # serialized_logbook = PopulatedLogbookSerializer(logbook)
+        # return Response(serialized_logbook.data, status=status.HTTP_200_OK)
