ac35f9ea9bfe887633bdf6878adfa6d660f41a8b
Hamza Butt <hamzambutt1@gmail.com>
2021-04-21 20:00:48 +0100

    welcome message and status on logbook page added - css added too

--- client/src/components/Logbook.js
+++ client/src/components/Logbook.js
@@ -25,20 +25,34 @@ const Logbook = () => {
 
   return (
     <>
-      { (logbook.length > 0) &&
-      <div>
-        {logbook.map(item => {
-          return <h3 key={item.id}>{item.name}</h3>
-        })}
-        {logbook.map(item => {
-          return item.food.map(i => {
-            return <h4 key={i.id}>{i.title}</h4>
-          })
-        })}
+      <div className="logbook-container">
+        <div className="logbook-box">
+          <div className="logbook-top">
+            <div className="logbook-welcome">
+              <h4>Welcome back {logbook[0].owner.first_name}</h4>
+            </div>
+            <div className="logbook-status">
+              <h4>You have {logbook.length} logbooks</h4>
+            </div>
+          </div>
+        </div>
       </div>
-      }
     </>
   )
 }
 
 export default Logbook
+
+
+// { (logbook.length > 0) &&
+//   <div>
+//     {logbook.map(item => {
+//       return <h3 key={item.id}>{item.name}</h3>
+//     })}
+//     {logbook.map(item => {
+//       return item.food.map(i => {
+//         return <h4 key={i.id}>{i.title}</h4>
+//       })
+//     })}
+//   </div>
+//     }
