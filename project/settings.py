a579a7fb2f937d9036bbbe6e761c3ba289e49b35
Hamza Butt <hamzambutt1@gmail.com>
2021-04-28 13:47:05 +0100

    installed django on heroku for deployment

--- project/settings.py
+++ project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,3 +152,5 @@ STATIC_URL = '/static/' # same with this
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend', "build", "static"),
 )
+
+django_on_heroku.settings(locals())
